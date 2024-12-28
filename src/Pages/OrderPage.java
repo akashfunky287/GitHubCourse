20673de754cfe6fd5acef693da8fb72b0c84843d
akashfunky287 <akashfunky287@gmail.com>
2024-12-28 11:23:06 +0400

    Updated order feature as per Naveen's review comments

--- src/Pages/OrderPage.java
+++ src/Pages/OrderPage.java
@@ -5,6 +5,8 @@ public class OrderPage {
 	public static void doOrder() {
 		
 		System.out.println("Order Page");
+		String orderId="123";
+		System.out.println(orderId);
 		
 	}
 
--- src/tests/OrderPageTest.java
+++ src/tests/OrderPageTest.java
@@ -2,9 +2,11 @@ package tests;
 
 public class OrderPageTest {
 	
+	//@Test
 	public void orderTest() {
 		
 		System.out.println(true);
+		//Assert.assertTrue(true,true);
 	}
 
 }
