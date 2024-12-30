25ad792c542d9f38f79c58e93d2bc869e6f92a3e
akashfunky287 <akashfunky287@gmail.com>
2024-12-30 16:07:53 +0400

    Third cherry commit

--- src/Pages/CherryPage.java
+++ src/Pages/CherryPage.java
@@ -8,6 +8,11 @@ public class CherryPage {
 		int sal=100;
 
 	}
+	
+	public void test() {
+		System.out.println("test feature");
+	}
 
 }
 //f5a261c (HEAD -> master) first cherry commit
+//96e0051 (HEAD -> master) second cherry commit
