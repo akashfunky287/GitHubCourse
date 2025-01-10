1cbd781389b15735136e93e037554a59065e0292
akashfunky287 <akashfunky287@gmail.com>
2025-01-10 12:12:33 +0400

    added cart feature

--- src/Pages/CartPage.java
+++ src/Pages/CartPage.java
@@ -3,8 +3,13 @@ package Pages;
 public class CartPage {
 	
 	public void cart() {
+<<<<<<< Updated upstream
 		String nameD2="cart dev 2";
 		System.out.println(nameD2);
+=======
+		String name="cart";
+		System.out.println(name);
+>>>>>>> Stashed changes
 	}
 
 }
