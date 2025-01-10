18151e034be0fc147a3ba03122f2d2041aa599a3
akashfunky287 <56388269+akashfunky287@users.noreply.github.com>
2025-01-10 12:32:53 +0400

    Create ClassicPage.java

---
+++ src/Pages/ClassicPage.java
@@ -0,0 +1,14 @@
+package Pages;
+
+public class ClassicPage {
+
+	public static void main(String[] args) {
+		int k= 100;
+		int k1= 200;
+		int div = k1/k;
+		System.out.println(div);
+    System.out.println("Bye");
+
+	}
+
+}
